263a4b1ae361515e5e0bb7eaff04f85d1146bf30
cassaram09 <cassaram09@gmail.com>
2018-11-14 18:41:04 -0500

    adjust blog post page

--- wp-content/themes/master/single.php
+++ wp-content/themes/master/single.php
@@ -1,29 +1,21 @@
-
 <?php get_header(); ?>
 
 <!-- COMPONENT // Hero -->
 <section class='hero -blog'>
   <div class='hero__content -blog'>
-    
   </div>
 </section>
 
 <section class='blog-content -bg-light'>
-
   <div class='container'>
-
 
     <div class='row blog-content__header'>
       <div class='col-12'>
-
-         <h2><?php the_title()?></h2>
-          <?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
-        
-
+        <h1><?php the_title()?></h1>
+        <?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
       </div>
     </div>
  
-
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
     <!-- Post Image (Optional) -->
@@ -38,40 +30,31 @@
             
       </div>
       <div class='col-1'></div>
-
     </div>
     <?php endif; ?>
 
     <div class='row'>
-
       <div class='col-12'>
 
-          <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-              
-            <div class="text -single">
-              <?php the_content(); ?>
-            </div>
+        <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
             
-            <?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+          <div class="text -single">
+            <?php the_content(); ?>
+          </div>
+          
+          <?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+          <?php edit_post_link('Edit post','','.'); ?>
             
-            <?php edit_post_link('Edit post','','.'); ?>
-              
-          </article>
+        </article>
 
-          <div class="tags"><?php the_tags( 'Tags: ', ' ', ''); ?></div>
-
+        <div class="tags"><?php the_tags( 'Tags: ', ' ', ''); ?></div>
       </div>
-
     </div>
     <?php endwhile; endif; ?>
 
   </div>
-
 </section>
-
 
 <?php include (TEMPLATEPATH . '/components/comp_related-posts.php' ); ?>
 
-
 <?php get_footer(); ?>
-
